c3875a99bf955ce199fba9afd92b3e449558527b
henriquelbk <loubackhenrique123@gmail.com>
2024-12-03 20:49:53 -0300

    .

--- integers.js
+++ integers.js
@@ -3,3 +3,22 @@
 var sum = function(num1, num2) {
     return num1 + num2;
 };
+
+// 128. Longest Consecutive Sequence
+
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums);
+    let longest = 0;
+
+    for (let n of nums) {
+        if (!numSet.has(n - 1)) {
+            let length = 1;
+
+            while (numSet.has(n + length)) {
+                length++;
+            }
+            longest = Math.max(longest, length);
+        }
+    }
+    return longest;    
+};
