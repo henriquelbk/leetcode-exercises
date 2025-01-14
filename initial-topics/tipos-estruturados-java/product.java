7c2d304148e050a3feb295c0f8d62b065a556050
henriquelbk <loubackhenrique123@gmail.com>
2025-01-13 21:26:58 -0300

    Métodos e Orientação à Objetos

--- initial-topics/tipos-estruturados-java/product.java
+++ initial-topics/tipos-estruturados-java/product.java
@@ -2,14 +2,38 @@ package app;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double total() {
+        return price * quantity;
+    }
+
+    public void updatePrice(double percentage){
+        price = price * (1.0 + percentage / 100.0);
     }
 
     public String toString() {
--- initial-topics/tipos-estruturados-java/program.java
+++ initial-topics/tipos-estruturados-java/program.java
@@ -3,11 +3,16 @@
 package app;
 
 public class Program {
+
     public static void main(String[] args) {
 
         var p1 = new Product("Laptop", 1000.0, 5);
         var p2 = new Product("Mouse", 120.0, 11);
 
+        p1.setPrice(1299.0);
+        p1.updatePrice(10.0);
+
+        System.out.println(p1.getPrice());
         System.out.println(p1);
         System.out.println(p2);
     }
