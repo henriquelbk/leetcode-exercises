9b1004ebe8b1f4adb6e9957eaac46e038afb68e9
henriquelbk <loubackhenrique123@gmail.com>
2025-04-29 22:35:06 -0300

    csharp initials

---
+++ initial-topics/tipos-estruturados-csharp/Product.cs
@@ -0,0 +1,20 @@
+namespace MyApp
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity =  quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", $" + Price.ToString("F2") + ", " + Quantity;
+        }
+    }
+}
---
+++ initial-topics/tipos-estruturados-csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Product("Laptop", 1000.0, 5);
+            var p2 = new Product("Headphone", 200.0, 2);
+            var p3 = new Product("Mouse", 80.0, 12);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+    }
+}
